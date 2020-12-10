b1f119c88e1e8554ef331ab1d37814cfe1d3fcd0
Dmarcano <dmarcan1@swarthmore.edu>
2020-12-10 02:20:17 -0500

    added keyboard buffer

--- src/chip8.rs
+++ src/chip8.rs
@@ -57,7 +57,9 @@ pub struct Chip8CPU {
     rng : rand::prelude::ThreadRng,
 
     /// the display buffer that is used to draw graphics
-    disp_buf : [u8 ; 64*32]
+    disp_buf : [u8 ; 64*32], 
+
+    keyboard : [u8; 16]
 }
 
 // public methods
@@ -70,6 +72,8 @@ impl  Chip8CPU{
         let mut memory : [u8; 4096] = [0; 4096]; 
         let stack = [0; 16];
         let disp_buf = [0; 32*64];
+        let keyboard = [0; 16];
+
 
         let rng = rand::thread_rng();
         let pc : u16 = START_ADDR as u16; 
@@ -91,7 +95,8 @@ impl  Chip8CPU{
             delay_timer, 
             sound_timer,
             rng,
-            disp_buf
+            disp_buf,
+            keyboard
         }
         
     }
@@ -371,12 +376,15 @@ mod tests{
         let exp_pc = START_ADDR as u16; 
         let exp_stack = [0; 16];
         let exp_disp_buf = [0; 32 * 64]; 
+        let exp_keyboard = [0; 16];
+
         
         check_fontset(&cpu.memory);
         assert_eq!(cpu.v, exp_v);
         assert_eq!(exp_pc, cpu.pc);
         assert_eq!(exp_stack, cpu.stack);
         assert_eq!(exp_disp_buf, cpu.disp_buf);
+        assert_eq!(exp_keyboard, cpu.keyboard);
 
         cpu.memory[START_ADDR + 1] = 10;
         cpu.stack[0] = 0x23;
@@ -390,6 +398,7 @@ mod tests{
         assert_eq!(exp_pc, cpu.pc);
         assert_eq!(exp_stack, cpu.stack);
         assert_eq!(exp_disp_buf, cpu.disp_buf);
+        assert_eq!(exp_keyboard, cpu.keyboard);
 
 
     }
