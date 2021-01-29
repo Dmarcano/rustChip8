600a63ab53721d1190b25420253cb3b049aa22ca
Dmarcano <dmarcan1@swarthmore.edu>
2021-01-29 15:08:18 -0500

    bug on multiple renders

--- wasm-chip8/www/index.js
+++ wasm-chip8/www/index.js
@@ -10,23 +10,36 @@ const ctx = canvas.getContext('2d')
 
 let isRunning = false; 
 
-const color = [255, 0, 0, 255];
+const CANVAS_COLOR = [255, 0, 0, 255];
 
-const roms = ["BC_test", "octojam2title", "danm8ku", "test_opcode" ]
+const ROMS = ["BC_test", "octojam2title", "danm8ku", "test_opcode" ]
 
 const main = () =>  { 
     let chip8 = wasm.WasmChip8.new(); 
     console.log(chip8)
-    let name = "octojam2title.ch8";
+    let default_rom = "octojam2title.ch8";
     
     const forward_btn = document.getElementById('forward-chip8');
     const start_btn = document.getElementById('start-chip8');
     const stop_btn = document.getElementById('stop-chip8');
 
+    const sel_element = create_rom_selection(); 
+
+    sel_element.addEventListener('change', (event) => { 
+        let rom_name = event.target.value; 
+        let file_extension = `${rom_name}.ch8`;
+
+        get_rom_file(file_extension).then(rom => {
+            chip8.reset();
+            chip8.load_rom_js(rom);
+            update_canvas(chip8);
+        });
+    });
+
 
     forward_btn.addEventListener("click", function() { 
             emulate_cycle(chip8);
-        })
+    });
 
     start_btn.addEventListener("click", function() { 
         isRunning = true; 
@@ -35,7 +48,7 @@ const main = () =>  {
         forward_btn.disabled = true; 
 
 
-    })
+    });
 
     stop_btn.addEventListener('click', function() { 
         isRunning = false;
@@ -45,7 +58,8 @@ const main = () =>  {
     })
 
 
-    get_rom_file(name).then((rom) => { 
+    get_rom_file(default_rom).then((rom) => { 
+        chip8.reset();
         chip8.load_rom_js(rom); 
         update_canvas(chip8);
 
@@ -61,8 +75,6 @@ const emulate_cycle = (chip8) => {
 
 
 const emulation_loop = (chip8) => { 
-
-
     if(isRunning) { 
         for(var i =0; i < 9; i++) {
             chip8.cycle(); 
@@ -107,6 +119,20 @@ const get_rom_file = async(name) => {
     let file = await fetch(`roms/${name}`); 
     let buff = await file.arrayBuffer(); 
     return new DataView(buff, 0, buff.byteLength); 
+}
+
+const create_rom_selection = () => { 
+    const sel_element = document.createElement('select');
+    const sel_div = document.getElementById('rom-select');
+    sel_element.name = "rom_drop"
+    sel_element.id = "rom-select"
+
+    const rom_select = ROMS.map(rom => {
+        return `<option value="${rom}">${rom}</option>`;
+    });
+    sel_element.innerHTML = rom_select;
+    window.onload = () => sel_div.appendChild(sel_element);
+    return sel_element; 
 }
 
 
